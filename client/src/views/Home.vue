78b23f125c2909a2b53132a07577b07712317869
alpharmi <gianni.s177@outlook.com>
2024-05-26 17:41:20 +1000

    optimisations

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -16,14 +16,14 @@
                     Import your Convene History to view your latest and previous pulls
                     for Character, Weapon, Standard, and Beginner banners.
                 </p>
-                <button @click="redirect('/convene')" class="bgbutton mt-5 p-1.5 w-max px-3 inlineimage"><img class="pr-2" src="/other/radiant_tide.webp">Convene Counter ></button>
+                <button @click="redirect('/convene')" class="bgbutton mt-5 p-1.5 w-max px-3 inlineimage"><img class="pr-2" src="/other/radiant_tide.webp" alt="radiant tide">Convene Counter ></button>
             </div>
             <div class="bgcontainer p-3 flex flex-col">
                 <p class="text-md pt-2">
                     Calculate the amount of Convenes needed to reach your next pity. 
                     Includes advanced filters to refund coral and more.
                 </p>
-                <button class="bgbutton mt-5 p-1.5 w-max px-3 inlineimage"><img class="pr-2" src="/other/afterglow_coral.webp">Coming Soon ></button>
+                <button class="bgbutton mt-5 p-1.5 w-max px-3 inlineimage"><img class="pr-2" src="/other/afterglow_coral.webp" alt="afterglow coral">Coming Soon ></button>
             </div>
             <div class="relative h-[8.8rem]">
                 <div class="absolute w-full h-full flex items-center justify-center z-10">
@@ -31,7 +31,7 @@
                 </div>
                 <div class="absolute flex flex-wrap gap-3 h-auto items-stretch justify-center blur-sm brightness-[0.2]">
                     <div v-for="title in ['characters', 'weapons', 'echos']" class="flex justify-between flex-col h-[8.8rem] w-[8.8rem] bgcontainer">
-                        <img :src="`/other/${title}.png`" class="object-cover h-[6.7rem]">
+                        <img :src="`/other/${title}.webp`" class="object-cover h-[6.7rem]" :alt="`${title}`">
                         <p class="w-full text-container font-[400] text-xl text-center bg-highlight h-8 content-center textshadow capitalize">{{ title }}</p>
                     </div>
                 </div>
