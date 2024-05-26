7d04b3bac3233c1c679763b934f9f1b3da18d020
alpharmi <gianni.s177@outlook.com>
2024-05-26 22:45:24 +1000

    Home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,7 +4,7 @@
             <div class="bgcontainer p-3">
                 <p class="text-2xl">Welcome to <span class="textgold">wuthering.app</span></p>
                 <p class="text-md pt-2">
-                    Currently includes Gacha Tracker, and general game information 
+                    Currently includes Pity Counter, and general game information 
                     for Wuthering Waves. Please note everything is still in development
                     and some features may not work as expected.
                 </p>
