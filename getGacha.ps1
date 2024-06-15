98c556d9cbe7de714de57c233f1894f4b9e36ff8
theREalpha <therealpha@proton.me>
2024-06-15 13:31:34 +0530

    Update getGacha.ps1

--- getGacha.ps1
+++ getGacha.ps1
@@ -14,9 +14,8 @@ $method = "automatic"
 
 #EGS Version or Fallback to Manual
 if (!$gamePath -or !$gachaLogPathExists) {
-    $currentUserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
     $muiCachePath = "Registry::HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache"
-    $filteredEntries = (Get-ItemProperty -Path $muiCachePath).PSObject.Properties | Where-Object { $_.Value -like "*wuthering*" }
+    $filteredEntries = (Get-ItemProperty -Path $muiCachePath).PSObject.Properties | Where-Object { $_.Value -like "*wuthering*" } | Where-Object {$_.Name -like "*client-win64-shipping.exe*"}
     if ($filteredEntries.Count -ne 0) {
         $gachaLogPath = ($filteredEntries[0].Name -split 'client-win64-shipping')[0] + "ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView"
     }
@@ -46,38 +45,38 @@ if (!$gamePath -or !$gachaLogPathExists) {
             Write-Output "Invalid game path. Please try again."
         }
         $gachaLogPath = $gamePath + '\Wuthering Waves Game\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView'
+	}
+}
+if (Test-Path ($gachaLogPath + "\debug.log") -PathType Leaf) {
+    Write-Output "Finding Gacha Url..."
+
+    Copy-Item -Path ($gachaLogPath + "\debug.log") -Destination ($gachaLogPath + "\debug_copy.log")
+    $cacheData = Get-Content ($gachaLogPath + "\debug.log")
+    Remove-Item -Path ($gachaLogPath + "\debug_copy.log")
+    $cacheDataLines = $cacheData -split '1/0/'
+    $url = ""
+
+    for ($i = $cacheDataLines.Length - 1; $i -ge 0; $i--) {
+        $line = $cacheDataLines[$i]
+
+        if ($line.Contains("https://aki-gm-resources-oversea.aki-game.net/aki/gacha/index.html#/record")) {
+            $url = (($line -split ': "')[1].replace('",', "")) + ("&wa_method=" + $method)
+
+            break
+        }
     }
-    if (Test-Path ($gachaLogPath + "\debug.log") -PathType Leaf) {
-        Write-Output "Finding Gacha Url..."
 
-        Copy-Item -Path ($gachaLogPath + "\debug.log") -Destination ($gachaLogPath + "\debug_copy.log")
-        $cacheData = Get-Content ($gachaLogPath + "\debug.log")
-        Remove-Item -Path ($gachaLogPath + "\debug_copy.log")
-        $cacheDataLines = $cacheData -split '1/0/'
-        $url = ""
-
-        for ($i = $cacheDataLines.Length - 1; $i -ge 0; $i--) {
-            $line = $cacheDataLines[$i]
-
-            if ($line.Contains("https://aki-gm-resources-oversea.aki-game.net/aki/gacha/index.html#/record")) {
-                $url = (($line -split ': "')[1].replace('",', "")) + ("&wa_method=" + $method)
-
-                break
-            }
-        }
-
-        if ($url) {
-            Write-Output " "
-            Write-Output $url
-            Set-Clipboard -Value $url
-            Write-Output " "
-            Write-Output "Gacha Url has been saved to clipboard."
-        }
-        else {
-            Write-Output "Unable to find Gacha Url. Please open Convene Records in game."
-        }
+    if ($url) {
+        Write-Output " "
+        Write-Output $url
+        Set-Clipboard -Value $url
+        Write-Output " "
+        Write-Output "Gacha Url has been saved to clipboard."
     }
     else {
         Write-Output "Unable to find Gacha Url. Please open Convene Records in game."
     }
+}
+else {
+    Write-Output "Unable to find Gacha Url. Please open Convene Records in game."
 }
